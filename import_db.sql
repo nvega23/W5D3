c9a5b32ef66a148cce12ac3e29b075ea95b2adde
Nestor Vega <vega.nestor1@gmail.com>
2022-11-02 10:40:42 -0700

    debugging

--- import_db.sql
+++ import_db.sql
@@ -1,19 +1,23 @@
-DROP table if exists users;
+PRAGMA foreign_keys = ON;
 
+
+DROP table if exists users;
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
-
-    question_id INTEGER NOT NULL,
-    FOREIGN KEY (question_id) REFERENCES questions(id)
+    lname TEXT NOT NULL
+    -- question_id INTEGER NOT NULL,
+    -- FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 DROP table if exists questions;
 CREATE TABLE questions(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
-    body TEXT NOT NULL
+    author_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 DROP table if exists question_follows;
@@ -38,7 +42,7 @@ CREATE TABLE replies (
     FOREIGN KEY (question_id) REFERENCES questions(id)
     FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
     FOREIGN KEY(user_id) REFERENCES users(id)
-    FOREIGN KEY (reply_body) REFERENCES questions(body)
+    -- FOREIGN KEY (reply_body) REFERENCES questions(body)
 
 );
 
@@ -48,25 +52,22 @@ CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
     question_likes BOOLEAN NOT NULL,
     -- TO REFERENCE
-    user_id INTEGER NOT NULL
-    question_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES (users(id))
-    FOREIGN KEY (question_id) REFERENCES (questions(id))
-
-
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Arthur', 'Miller'),
+  ('Eugene', 'Neill');
 
--- INSERT INTO
---   questions (title, body)
--- VALUES
---   ('whatever', 'whats my name'),
---   ('i dont know', 'where am i');
-
--- INSERT INTO
---   users (fname, lname)
--- VALUES
---   ('Arthur', 'Miller', ),
---   ('Eugene', 'Neill');
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('whatever', 'whats my name', (SELECT id FROM users WHERE fname = 'Arthur')),
+  ('i dont know', 'where am i', (SELECT id FROM users WHERE fname = 'Eugene'));
